ff99eaa671dbba86106e41fb5623e8be60cf6ea3
gapur <kaloycant@gmail.com>
2023-02-07 22:00:12 +0100

    #1 - project setup

---
+++ frontend/src/pages/index.tsx
@@ -0,0 +1,15 @@
+import { NextPage } from 'next';
+import Head from 'next/head';
+
+const Home: NextPage = () => {
+  return (
+    <>
+      <Head>
+        <title>iMessage</title>
+      </Head>
+      <main>Hello home page</main>
+    </>
+  );
+};
+
+export default Home;
