8c9b9704afc80c5c595514a684e07338af977c9d
gapur <kaloycant@gmail.com>
2023-02-08 23:26:53 +0100

    Creating Users (NextAuth Google Provider & useSession hook)

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,5 +1,16 @@
 import type { AppProps } from 'next/app';
+import { SessionProvider } from 'next-auth/react';
+import { ChakraProvider } from '@chakra-ui/react';
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+export default function App({
+  Component,
+  pageProps: { session, ...pageProps },
+}: AppProps) {
+  return (
+    <SessionProvider session={session}>
+      <ChakraProvider>
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </SessionProvider>
+  );
 }
--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,13 +1,26 @@
 import { NextPage } from 'next';
+import { signIn, signOut, useSession } from 'next-auth/react';
 import Head from 'next/head';
 
 const Home: NextPage = () => {
+  const { data } = useSession();
+
+  console.log('HERE IS DATA', data);
+
   return (
     <>
       <Head>
         <title>iMessage</title>
       </Head>
-      <main>Hello home page</main>
+      <main>
+        {data?.user ? (
+          <button onClick={() => signOut()}>Sign Out</button>
+        ) : (
+          <button onClick={() => signIn('google')}>Sign In</button>
+        )}
+        <br />
+        {data?.user?.name}
+      </main>
     </>
   );
 };
